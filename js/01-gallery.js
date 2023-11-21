1d860feca246106a4236cc98e6f8ffc043cf1f2b
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2023-11-22 00:23:17 +0200

    start part 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+const images = galleryItems
+  .map(
+    ({ original, description, preview }) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>`
+  )
+  .join(" ");
+
+list.insertAdjacentHTML("beforeend", images);
+
+list.addEventListener("click", onClick);
+
+function onClick(eve) {
+  eve.preventDefault();
+  if (!eve.target.dataset) {
+    return;
+  }
+  console.log(eve.target.dataset.source);
+}
