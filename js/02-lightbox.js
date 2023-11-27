3479571c3fed39121f4363461fccc8b128216d60
SergeyPavluchenko <s.pavluchenko90@gmail.com>
2023-11-27 23:01:40 +0200

    done image gallery 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// import SimpleLightbox from "simplelightbox";
 // Change code below this line
-
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+console.log(list);
+
+const images = galleryItems
+  .map(
+    ({ description, original, preview }) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+  )
+  .join(" ");
+
+list.insertAdjacentHTML("beforeend", images);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+gallery.on("show.simplelightbox", function () {});
